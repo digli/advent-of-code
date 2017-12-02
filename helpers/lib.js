02b9a69b6ca8047c534ecad6af78f4301f2c9af6
Elliot Jalgard <elliot.j@live.se>
2017-12-02 19:30:27 +0100

    Add some helper functions for quicker solving

---
+++ helpers/lib.js
@@ -0,0 +1,8 @@
+// Just some copy-paste helpers for a quicker start
+const sum = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const mul = (a, b) => a * b;
+const div = (a, b) => a / b;
+const mod = (a, b) => ((a % b) + b) % b;
+
+const input = document.body.textContent.trim().split('\n');
