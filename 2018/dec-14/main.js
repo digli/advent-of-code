ab7d2da38fe1cf70427dad49ff46a6b2c16f320d
digli <elliot.j@live.se>
2018-12-14 08:35:40 +0100

    Actually reading the description makes solving the puzzle easier

--- 2018/dec-14/main.js
+++ 2018/dec-14/main.js
@@ -29,7 +29,7 @@ const getIterations = (initialState, recipe) => {
     const state = [...initialState];
     const elfPos = [...Array(initialState.length).keys()];
 
-    for (let iteration = 0; ; iteration++) {
+    for (;;) {
         const sum = elfPos.map(pos => state[pos])
             .reduce((a, b) => a + b);
 
@@ -39,14 +39,18 @@ const getIterations = (initialState, recipe) => {
             elfPos[i] %= state.length;
         }
 
-        const foundRecipe = state.slice(state.length - (recipe.length + 3)).join('')
+        const foundRecipe = state.slice(state.length - (recipe.length + 2)).join('')
             .includes(recipe);
         if (foundRecipe) {
-            return iteration;
+            return (
+                state.slice(state.length - (recipe.length + 1)).join('') === recipe
+                    ? state.length - recipe.length
+                    : state.length - recipe.length - 1
+            )
         }
 
     }
 }
 
 console.log('The answer to puzzle 1 is', getScore(initialState, input));
-console.log('The answer to puzzle 2 is not', getIterations(initialState, String(input)));
+console.log('The answer to puzzle 2 is', getIterations(initialState, String(input)));
