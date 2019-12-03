0a6c8357aa653ab92e4bc110fda70c9be6576d38
digli <elliot.j@live.se>
2019-12-03 16:43:34 +0100

    Fix 03

--- 2018/dec-20/main.js
+++ 2018/dec-20/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const fs = require('fs');
+const input = String(fs.readFileSync('input.txt')).trim();
+
 const tests = [
     { input: '^WNE$', answer: 3 },
     { input: '^ENWWW(NEEE|SSE(EE|N))$', answer: 10 },
@@ -51,11 +54,10 @@ const getOrCreateRoom = (rooms, currentRoom, x, y) => {
     return room;
 }
 
-// TODO: Figure out what return value to use
-const branch = (directions, rooms, origin) => {
-    const currentBranches = [];
+const branch = (directions, offset, rooms, origin) => {
+    let currentRoom = origin;
 
-    for (let i = 0; i < directions.length; i++) {
+    for (let i = offset; i < directions.length; i++) {
         switch (directions[i]) {
             case 'N': {
                 currentRoom = getOrCreateRoom(rooms, currentRoom, currentRoom.x, currentRoom.y - 1);
@@ -74,33 +76,97 @@ const branch = (directions, rooms, origin) => {
                 break;
             }
             case '(': {
-                const { offset, branches } = branch(directions, i);
-                for (const r of branches) {
-                    currentRoom.addNeighbor(r);
-                }
+                const offset = branch(directions, i + 1, rooms, currentRoom);
 
-                i += offset;
+                i = offset;
                 break;
             }
             case ')': {
-                return {
-                    currentBranches,
-                    offset: i
-                };
+                return i;
             }
             case '|': {
-                // currentBranches.push(origin);
-                // currentRoom = origin;
+                currentRoom = origin;
             }
         }
     }
+
+    return directions.length;
 }
 
 const buildMap = (input) => {
-    const directions = input.trim().substring(1, input.length - 2);
+    const directions = input.trim().substring(1, input.length - 1);
     const rooms = new Map();
     const start = new Room(0, 0);
 
     rooms.set(start.id, start);
-    branch(directions, rooms, start);
+    branch(directions, 0, rooms, start);
+    return start;
 }
+
+/*
+const drawMap = (start, offset) => {
+    const grid = [];
+    const rooms = new Set();
+
+    const addRoom = (room) => {
+        if (rooms.has(room.id)) { return; }
+        rooms.add(room.id);
+        const x = room.x * 2 + offset;
+        const y = room.y * 2 + offset;
+
+        for (let i = -1; i < 2; i++) {
+            if (!grid[y + i]) grid[y + i] = [];
+
+            for (let j = -1; j < 2; j++) {
+                grid[y + i][x + j] = grid[y + i][x + j] || '#';
+            }
+        }
+
+        grid[y][x] = '.';
+        if (room.north) { grid[y - 1][x] = ' '; addRoom(room.north); }
+        if (room.east) { grid[y][x + 1] = ' '; addRoom(room.east); }
+        if (room.south) { grid[y + 1][x] = ' '; addRoom(room.south); }
+        if (room.west) { grid[y][x - 1] = ' '; addRoom(room.west); }
+    }
+
+    addRoom(start);
+
+    return grid.map(line => line.join('')).join('\n');
+}
+*/
+
+const visitAllRooms = (start) => {
+    const visited = new Set();
+    const queue = [{ room: start, score: 0 }];
+    let maxScore = 0;
+    let farOff = 0;
+
+    do {
+        const { room, score } = queue.shift();
+        if (visited.has(room.id)) { continue; }
+
+        if (score >= 1000) {
+            farOff += 1;
+        }
+
+        maxScore = Math.max(score, maxScore);
+
+        visited.add(room.id);
+        for (const dir of ['north', 'east', 'south', 'west']) {
+            if (room[dir]) {
+                queue.push({
+                    room: room[dir],
+                    score: score + 1
+                });
+            }
+        }
+    } while (queue.length > 0);
+
+    return { maxScore, farOff };
+}
+
+const startingRoom = buildMap(input);
+const { maxScore, farOff } = visitAllRooms(startingRoom);
+
+console.log('The answer to puzzle 1 is', maxScore);
+console.log('The answer to puzzle 2 is', farOff);
--- 2019/03.js
+++ 2019/03.js
@@ -1,42 +1,66 @@
-const input = document.body.textContent.split('\n').map(row => row.split(','));
-
 const manhattan = (p1, p2) => Math.abs(p2.x - p1.x) + Math.abs(p2.y - p1.y);
 
-const run = (input) => {
+const execute = instructions => {
     const visitedCoordinates = new Map();
-    const collisions = [];
+    let x = 0, y = 0, distanceTravelled = 0;
 
-    for (const row of input) {
-        let x = 0, y = 0;
+    for (const instruction of instructions) {
+        const direction = instruction[0];
+        const length = Number(instruction.slice(1));
 
-        for (const instruction of row) {
-            const direction = instruction[0];
-            const length = Number(instruction.slice(1));
+        for (let i = 0; i < length; i++) {
+            switch (direction) {
+                case 'U': y -= 1; break;
+                case 'R': x += 1; break;
+                case 'D': y += 1; break;
+                case 'L': x -= 1; break;
+            }
 
-            for (let i = 0; i < length; i++) {
-                switch (direction) {
-                    case 'U': y -= 1; break;
-                    case 'R': x += 1; break;
-                    case 'D': y += 1; break;
-                    case 'L': x -= 1; break;
-                }
+            distanceTravelled += 1;
+            const key = `${x}:${y}`;
 
-                const key = `${x}:${y}`;
-                const point = { x, y };
-
-                if (visitedCoordinates.has(key)) {
-                    collisions.push(point);
-                } else {
-                    visitedCoordinates.set(key, point);
-                }
+            if (!visitedCoordinates.has(key)) {
+                const point = { x, y, distanceTravelled };
+                visitedCoordinates.set(key, point);
             }
         }
     }
 
-    const origo = { x: 0, y: 0 };
-    const min = collisions.map(point => manhattan(point, origo)).reduce(Math.min, Infinity);
-
-    return min;
+    return visitedCoordinates;
 }
 
+const run = input => {
+    const [path1, path2] = input.map(execute);
+    const collisions = [];
+
+    for (const [key, point] of path1) {
+        if (path2.has(key)) {
+            collisions.push(point);
+        }
+    }
+
+    const origo = { x: 0, y: 0 };
+    const minManhattan = collisions
+        .map(point => manhattan(point, origo))
+        .reduce((a, b) => Math.min(a, b), Infinity);
+
+    const minDistanceTravelled = collisions
+        .map(({ x, y }) => {
+            const key = `${x}:${y}`;
+            const d1 = path1.get(key).distanceTravelled;
+            const d2 = path2.get(key).distanceTravelled;
+
+            return d1 + d2;
+        })
+        .reduce((a, b) => Math.min(a, b), Infinity);
+
+    return { minManhattan, minDistanceTravelled };
+}
+
+const input = document.body.textContent.split('\n').map(row => row.split(','));
+// const testInput1 = ['R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83'].map(x => x.split(','));
+// const testInput2 = ['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'].map(x => x.split(','));
+
 console.log(run(input));
+// console.log(run(testInput1));
+// console.log(run(testInput2));
